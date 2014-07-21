d30123973d8ff695f7cdd367172f4adb2f676262
mhpob <obrien@umces.edu>
2014-07-21 17:27:27 -0400

    Create act_data.R

---
+++ act_data.R
@@ -0,0 +1,34 @@
+setwd('p:/obrien/biotelemetry/detections')
+source('detection_sort.R')
+
+act <- read.csv('active transmitters 6-25-14.csv', header = T, stringsAsFactors = F)
+
+# Filter for ID'ed detections that aren't Dave's
+id <- filter(act, Tag.ID.Code.Standard %in% detects$transmitter,
+                  !Tag.ID.Code.Standard %in% paste('A69-1601', seq(25434, 25505), sep = '-'))
+
+id <- merge(detects, id[, c(1:2, 14:15)],
+           by.x = c('transmitter', 'trans.num'),
+           by.y = c('Tag.ID.Code.Standard', 'ID.Standard'))
+
+unid <- filter(detects, 
+               transmitter %in% setdiff(detects$transmitter, act$Tag.ID.Code.Standard))
+
+j <- split(id, id$Primary.Researcher)
+
+
+stdate <- ymd(20140605)
+enddate <- ymd(20140717) + days(1)
+
+for(i in seq(length(j))){
+j[[i]] <- j[[i]][c(1,3:7)]
+j[[i]][7:10] <- NA
+j[[i]] <- j[[i]][c(3, 2, 1, 7:10, 4:6)]
+names(j[[i]]) <- c('Date and Time (UTC)', 'Receiver', 'Transmitter', 'Transmitter Name',
+                   'Transmitter Serial', 'Sensor Value', 'Sensor Unit', 'Station Name',
+                   'Latitude', 'Longitude')
+j[[i]] <- j[[i]][order(j[[i]][3], j[[i]][1]),]
+j[[i]] <- j[[i]][j[[i]][,1] >= stdate & j[[i]][,1] <= enddate,]
+write.csv(j[[i]], file = paste(gsub(' ', '', names(j[i])), Sys.Date(),'.csv', 
+                               sep = ''), row.names = F)
+}
