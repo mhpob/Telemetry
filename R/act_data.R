d475d35bef63200ce591011886893ad580097b44
mhpob <mikeob9@gmail.com>
2014-09-22 16:10:47 -0400

    ACT Data.

--- R/act_data.R
+++ R/act_data.R
@@ -9,9 +9,6 @@
 #' 
 #' @param directory String passed on to \code{vemsort}. Location of CSV data,
 #'    which defaults to current wd.
-#' @param ACT String. Location of ACT's 'Active transmitters M-D-YY.csv' sheet.
-#'    File must be pre-converted from XLSX to CSV in order to work. Defaults to
-#'    a file with "Active" in the name within listed directory.
 #' @param my.trans Numeric vector. Tag ID codes that you want removed prior
 #'    to distribution.
 #' @param false.det Numeric vector passed on to \code{vemsort}. Contains tag ID
@@ -23,36 +20,28 @@
 #'    detections. Defaults to Jan 1, 2000.
 #' @param end Numeric. Date in ymd form to stop pulling detections. Defaults to
 #'    current system date.
-#' @return Outputs are CSV files in the form of ResearcherCurrentate.csv and a
+#' @return Outputs are CSV files in the form of ResearcherCurrentdate.csv and a
 #'    data frame containing detections of unidentified codes. The CSV files
 #'    will be found in your current working directory by default.
 #' @export
 #' @examples
 #' ACTsplit('C:/Users/mypcname/Documents/Vemco/Vue/ReceiverLogs')
-#' # Specify a different location for ACT transmitters and choose detections
-#' # from April 1, 2014 up to and including August 1, 2014.
+#' # Choose detections from April 1, 2014 up to and including August 1, 2014.
 #' ACTsplit('C:/Users/MYPCNAME/Documents/Vemco/Vue/ReceiverLogs',
-#'          'C:/Users/MYPCNAME/Documents/Active transmitters 9-2-14.csv',
 #'            start = 20140401, end = 20140801)
 
-ACTsplit <- function(directory = getwd(),
-              ACT = paste(directory,list.files(directory, 'Active'), sep = "/"),
-              my.trans = NULL, false.det = NULL, write = TRUE, out = NULL,
-              start = 20000101, end = Sys.Date()){
+ACTsplit <- function(directory = getwd(), my.trans = NULL, false.det = NULL,
+                     write = TRUE, out = NULL,
+                     start = 20000101, end = Sys.Date()){
   
   detects <- if(is.data.frame(directory)){
     directory
     } else{
       vemsort(directory, false.det)
     }
-  act <- if(is.data.frame(ACT)){
-    ACT
-    } else{
-      read.csv(ACT, header = T, stringsAsFactors = F)
-    }
-
+  
   # Filter for ID'ed detections that aren't yours
-  id <- dplyr::filter(act, Tag.ID.Code.Standard %in% detects$transmitter,
+  id <- dplyr::filter(ACTtrans, Tag.ID.Code.Standard %in% detects$transmitter,
                       !Tag.ID.Code.Standard %in% my.trans)
   
   id <- merge(detects, id[, c(1:2, 14:15)],
@@ -61,7 +50,7 @@ ACTsplit <- function(directory = getwd(),
   
   unid <- dplyr::filter(detects,
                         transmitter %in% setdiff(detects$transmitter,
-                                                 act$Tag.ID.Code.Standard))
+                                                 ACTtrans$Tag.ID.Code.Standard))
   
   j <- split(id, id$Primary.Researcher)
   
