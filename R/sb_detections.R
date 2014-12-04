7e1f473e894e21acd0e1cced5ca41f883d64a2a8
mhpob <mikeob9@gmail.com>
2014-12-04 14:46:20 -0500

    Uncomment, allow for new data and move.

--- R/sb_detections.R
+++ R/sb_detections.R
@@ -1,33 +1,28 @@
-# library(TelemetryR); library(lubridate); library(dplyr)
-# 
-# # False detections as determined by VEMCO
-# false <- c('37119', '64288')
-# detects <- vemsort('p:/obrien/biotelemetry/detections', false)
-# 
-# # Some data from MD DNR came in with location data missing
-# # dnr <- data.frame(receiver = c('VR2W-106474', 'VR2W-102036', 'VR2W-106473',
-# #                                'VR2W-106478'),
-# #                 station1 = c('Kent Island A', 'Kent Island B', 'Kent Island C',
-# #                              'Kent Island D'),
-# #                   lat1 = c(38.9953333, 38.9913167, 38.9841500,
-# #                            38.9799167),
-# #                   long1 = c(-76.3995333, -76.3902333, -76.3727833,
-# #                             -76.3539667),
-# #                   stringsAsFactors = F)
-# #   
-# # detects <- merge(detects, dnr, all.x = T)
-# # detects$station <- ifelse(is.na(detects$station), detects$station1,
-# #                           detects$station)
-# # detects$lat <- ifelse(is.na(detects$lat), detects$lat1, detects$lat)
-# # detects$long <- ifelse(is.na(detects$long), detects$long1, detects$long)
-# # detects <- detects[,-c(9:11)]
-# # detects <- detects[,c(2,1,3:8)]
-# # 
-# # dnr <- ACTsplit(j,
-# #       ACT = 'p:/obrien/biotelemetry/detections/active transmitters 9-2-14.csv',
-# #       my.trans = paste('A69-1601', seq(25434, 25505), sep = '-'),
-# #       out = 'p:/obrien/biotelemetry')
+library(TelemetryR); library(lubridate); library(dplyr)
+
+# False detections as determined by VEMCO
+false <- c('37119', '64288')
+detects <- vemsort('p:/obrien/biotelemetry/detections', false)
+
+# Some data from MD DNR came in with location data missing
+# dnr <- data.frame(receiver = c('VR2W-106474', 'VR2W-102036', 'VR2W-106473',
+#                                'VR2W-106478'),
+#                 station1 = c('Kent Island A', 'Kent Island B', 'Kent Island C',
+#                              'Kent Island D'),
+#                   lat1 = c(38.9953333, 38.9913167, 38.9841500,
+#                            38.9799167),
+#                   long1 = c(-76.3995333, -76.3902333, -76.3727833,
+#                             -76.3539667),
+#                   stringsAsFactors = F)
 #   
+# detects <- merge(detects, dnr, all.x = T)
+# detects$station <- ifelse(is.na(detects$station), detects$station1,
+#                           detects$station)
+# detects$lat <- ifelse(is.na(detects$lat), detects$lat1, detects$lat)
+# detects$long <- ifelse(is.na(detects$long), detects$long1, detects$long)
+# detects <- detects[,-c(9:11)]
+# detects <- detects[,c(2,1,3:8)]
+# 
 #     
 # arr <- function(part){grepl(part, detects[,4], ignore.case = T)}
 #   
@@ -47,16 +42,31 @@
 #                      c('CC LS', 'LC2', 'NCD', 'NN 1ER FWS',
 #                        'NN 22 NOAA SP', 'NN DANGER FWS', 'Y wat'), 'Navy',
 #                        'Other')))))))))))))
-#   
-# 
-# secor.sb <- filter(detects, trans.num >= 25434 & trans.num <= 25505)
-# 
-# tag.data <- read.csv('p:/obrien/biotelemetry/striped bass/taggingdata.csv',
-#                      header = T, stringsAsFactors = F)
-# tag.data$Date <- mdy(tag.data$Date, tz = 'America/New_York')
-# tag.data <- tag.data[, c(1,2,4,5,7)]
-# names(tag.data) <- c('tag.date', 'trans.num', 'size', 'length', 'weight')
-# 
-# secor.sb <- tbl_df(merge(secor.sb, tag.data, all.x = T))
-# 
-# rm(tag.data)
+  
+
+secor.sb <- filter(detects, trans.num >= 25434 & trans.num <= 25533)
+
+tag.data <- read.csv('p:/obrien/biotelemetry/striped bass/taggingdata.csv',
+                     header = T, stringsAsFactors = F)
+tag.data$Date <- mdy(tag.data$Date, tz = 'America/New_York')
+tag.data <- tag.data[, c(1, 2, 5, 7, 8)]
+names(tag.data) <- c('tag.date', 'trans.num', 'length', 'weight', 'sex')
+
+# we reused tag A69-1601-25465 on 2014-10-30. Need to split tagging data to
+# reflect this.
+firsttagging.25465 <- secor.sb %>% 
+  filter(trans.num == 25465, date.utc <= '2014-06-15') %>%
+  merge(tag.data[32,], all.x = T)
+
+secondtagging.25465 <- secor.sb %>% 
+  filter(trans.num == 25465, date.utc >= '2014-10-29') %>%
+  merge(tag.data[101,], all.x = T)
+
+secor.sb <- secor.sb %>% 
+  filter(trans.num != 25465) %>% 
+  merge(tag.data, all.x = T) %>%
+  rbind(firsttagging.25465, secondtagging.25465) %>%
+  tbl_df()
+
+
+rm(tag.data, firsttagging.25465, secondtagging.25465)
