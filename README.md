e7c94e4ee81f513381105f8a2c945c617dc31a3e
mhpob <mikeob9@gmail.com>
2015-04-24 16:03:30 -0400

    Update for ACT 20150415.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ TelemetryR
 Aggregate, separate, and disperse detections.
 
 Package includes current [A.C.T. database](http://www.theactnetwork.com) as of
-March 24, 2015.
+April 15, 2015.
--- data-raw/ACTtrans_build.R
+++ data-raw/ACTtrans_build.R
@@ -1,4 +1,4 @@
-active <- read.csv('data-raw/Active transmitters 3-24-15.csv', header = T,
+active <- read.csv('data-raw/Active transmitters 4-15-15.csv', header = T,
                      stringsAsFactors = F)
 active$Status <- 'Active'
 archived <- read.csv('data-raw/Archived transmitters 3-24-15.csv', header = T,
