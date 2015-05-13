8a85d44a15aa0937106c70f742d9106731ecfda2
mhpob <mikeob9@gmail.com>
2015-05-13 11:52:06 -0400

    Interpolator now works.

--- R/CBinterp.R
+++ R/CBinterp.R
@@ -1,4 +1,5 @@
 library(gdistance); library(rgdal); library(dplyr)
+library(geoR); library(SpatialTools)
 
 ### Data Entry -----------------------------------------------------------------
 ## Shapefile, reproject from meters to kilometers
@@ -6,10 +7,15 @@ chesapeake <- readOGR('c:/users/secor lab/desktop/gis products/erin',
                 'reporting_regions_new')
 chesapeake <- spTransform(chesapeake,
                           CRS('+proj=utm +zone=18 +datum=NAD83 +units=km'))
-
+# test.data <- read.csv('c:/users/secor lab/downloads/2005_spring_seasonal_water quality data_cbp.csv')
+# coordinates <- test.data[,c(2,1)]
+# data <- test.data$SALINITY
+# res <- 2
 
 
 CBinterp <- function(data, coordinates, res){
+  if(is.data.frame(data) == F) data <- as.data.frame(data)
+
   water_qual <- SpatialPointsDataFrame(coords = coordinates,
                                        data = data,
                                        proj4string = CRS('+proj=longlat'))
@@ -19,7 +25,8 @@ CBinterp <- function(data, coordinates, res){
     mutate(reasting = res * round(water_qual@coords[, 1] / res),
            rnorthing = res * round(water_qual@coords[, 2] / res)) %>%
     group_by(reasting, rnorthing) %>%
-    summarize(median = median(data))
+    summarize(median = median(data)) %>%
+    as.data.frame()
   water_qual@coords <- as.matrix(water_qual@data[, c('reasting','rnorthing')])
 
   ## Make grid to interpolate over, make into spatial object with same projection
@@ -47,38 +54,37 @@ CBinterp <- function(data, coordinates, res){
 
   ## Create transition matrix that represets a pairwise product of
   ## cells' conductance
-  cond.func <- function(x) x[1] * x[2]
-  ches.trans <- transition(ches.ras, cond.func, 8)
+  ches.trans <- transition(ches.ras, function(x) x[1] * x[2], 8)
   ches.trans <- geoCorrection(ches.trans)
 
   dist <- costDistance(ches.trans, water_qual)
-  dist <- as.matrix(as.dist(dist, diag = TRUE, upper = TRUE))
+  dist <- as.matrix(as.dist(dist, diag = T, upper = T))
 
   ### Interpolate ----------------------------------------------------------------
-  library(geoR); library(SpatialTools)
+
   # Use transition matrix to calculate corrected distances (as the fish swim)
   pred.dist <- costDistance(ches.trans, grid_water)
   pred.dist <- as.matrix(as.dist(pred.dist, diag = TRUE, upper = TRUE))
 
   # Calculate distances between observed and predicted values
-  op.dist <- costDistance(ches.trans, cbpwq, grid_water)
+  op.dist <- costDistance(ches.trans, water_qual, grid_water)
   op.dist <- as.matrix(op.dist, diag = TRUE, upper = TRUE)
 
 
   # Fit geostatistical model for the temp data
-  vg <- variog(coords = cbpwq@data[, c('reasting','rnorthing')],
-               data = cbpwq@data[, c('reasting','rnorthing', v)],
+  vg <- variog(coords = water_qual@data[, c('reasting','rnorthing')],
+               data = water_qual@data[, c('reasting','rnorthing', 'median')],
                max.dis = 600, dists.mat = dist)
   # ML fit
   vpar <- maxlik.cov.sp(as.matrix(cbind(1,
-                              cbpwq@data[, c('reasting','rnorthing')])),
-                  cbpwq@data[, v],
-                  coords = as.matrix(cbpwq@data[, c('reasting','rnorthing')]),
-                  sp.type = "matern", range.par = 600, error.ratio = 0.5,
-                  D = dist, reml = T)
+              water_qual@data[, c('reasting','rnorthing')])),
+              water_qual@data[, 'median'],
+              coords = as.matrix(water_qual@data[, c('reasting','rnorthing')]),
+              sp.type = "matern", range.par = 600, error.ratio = 0.5,
+              D = dist, reml = T)
 
   # Define spatial structure of prediction matrix from fitted spatial model
-  V0 <- cov.sp(coords = as.matrix(cbpwq@data[, c('reasting','rnorthing')]),
+  V0 <- cov.sp(coords = as.matrix(water_qual@data[, c('reasting','rnorthing')]),
                sp.type = "matern", sp.par = vpar$sp.par,
                error.var = vpar$error.var, smoothness = vpar$smoothness,
                finescale.var = 0,
@@ -86,17 +92,15 @@ CBinterp <- function(data, coordinates, res){
                D = dist, Dp = pred.dist, Dop = op.dist)
 
   # Apply spatial structure nd model to predict values
-  krige <- krige.uk(cbpwq@data[, v], V = V0$V, Vop = V0$Vop, Vp = V0$Vp,
+  krige <- krige.uk(water_qual@data[, 'median'],
+                    V = V0$V, Vop = V0$Vop, Vp = V0$Vp,
                     X = as.matrix(cbind(1,
-                                  cbpwq@data[, c('reasting','rnorthing')])),
+                              water_qual@data[, c('reasting','rnorthing')])),
                     Xp = as.matrix(cbind(1, grid_water@data[,1:2])), nsim = 0)
-  grid_water[[v]] <- krige$pred
-  grid_water[[paste(v, "se", sep=".")]] <- krige$mspe
+  grid_water[['value']] <- krige$pred
+  grid_water[['se']] <- krige$mspe
+  grid_water <- spTransform(grid_water, CRS('+proj=longlat'))
+  grid_water@data <- cbind(grid_water@coords,
+                           grid_water@data[, c('value', 'se')])
+  grid_water
 }
-
-
-
-grid_water <- spTransform(grid_water, CRS('+proj=longlat'))
-grid_water@data <- cbind(grid_water@coords, grid_water@data[, 4:9])
-
-write.csv(grid_water@data, 'interpolated.csv')
