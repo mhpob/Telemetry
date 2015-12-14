b79789c86b61a9789c4a623fe007d983164f24c4
mhpob <mikeob9@gmail.com>
2015-12-14 14:08:52 -0500

    Allow calculation using presence/absence.

--- R/quo_an.R
+++ R/quo_an.R
@@ -1,24 +1,28 @@
-data <- read.csv('p:/obrien/biotelemetry/csi/listening/activedata.csv',
-                 stringsAsFactors = F)
 
-env <- data[data$Type == 'B', 'Temp']
-det <- data[data$Type == 'B', 'Detections']
-bin_width <- 1
-
-quo_an <- function(env, det, bin_width = 1){
-  # Create grouping claesses
+quo_an <- function(env, det, bin_width = 1, pres_abs = F){
+  # Create grouping classes.
   classes <- cut(env, seq(floor(range(env)[1]),
                          ceiling(range(env)[2]),
                          bin_width))
 
-  # Aggregate by grouping classes.
-  fish <- aggregate(det ~ classes, FUN = sum)
+  # Aggregate by environmental classes.
+  if(pres_abs == T){
+    # Presence/Absence
+    fish <- data.frame(det, classes)
+    fish <- fish[det > 0,]
+    fish <- aggregate(det ~ classes, data = fish, FUN = length)
+  } else{
+    # Incidence
+    fish <- aggregate(det ~ classes, FUN = sum)
+  }
+
   station <- aggregate(env ~ classes, FUN = length)
 
-  q_an <- merge(fish, station)
+  q_an <- merge(fish, station, all = T)
+  q_an[is.na(q_an)] <- 0
 
-  q_an$pse <- q_an$env/sum(q_an$env)
   q_an$pme <- q_an$det/sum(q_an$det)
+  q_an$pse <- q_an$env/sum(q_an$env)
 
   q_an$qe <- q_an$pme/q_an$pse
 
