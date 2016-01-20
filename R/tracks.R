8de9a9307037e8238967ce49641cce22c3fe6a08
mhpob <mikeob9@gmail.com>
2016-01-20 17:38:07 -0500

    track() documentation.

--- R/tracks.R
+++ R/tracks.R
@@ -1,4 +1,19 @@
-
+#' Create coarse movement tracks.
+#'
+#' \code{tracks} returns locations (station, array, etc.) in the order in which
+#' they were visited.
+#'
+#' This function orders detection data by date/time and only returns detections
+#' where the fish moved from one identified location to another.
+#'
+#' @param data Data frame. Location of date/time and identifier values.
+#' @param dates character. Column name of date/time values within \code{data}.
+#' @param ids character. Column name(s) of identifiers within \code{data}.
+#'    Multiple names may be provided in a vector: c('station', 'lat', 'long'),
+#'    e.g.
+#' @return Output is a data frame with date/times at unique combinations of
+#'    provided IDs.
+#' @export
 
 track <- function(data, dates, ids){
   data <- data[order(data.frame(data)[, dates]), c(dates, ids)]
