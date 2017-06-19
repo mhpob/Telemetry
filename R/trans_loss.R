43f007243251556f4e303311d3cc3f596837696a
mhpob <mikeob9@gmail.com>
2017-06-19 13:11:45 -0400

    trans_loss documentation.

--- R/trans_loss.R
+++ R/trans_loss.R
@@ -1,4 +1,23 @@
-#' @export trans_loss
+#' Calculate number of transmitters remaining over time
+#'
+#' \code{trans_loss} returns the number of transmitters (or other group) remaining
+#' each day of a given time period
+#'
+#' This function finds the most-recent date of detection for each group (i.e.,
+#' transmitter/fish). For each date in a given range, it then counts the number
+#' of fish that have a more-recent final detection date.
+#'
+#' @param data Data frame. Detection data including date and time of detection,
+#'    as well as a group identifier.
+#' @param dates Date- or POSIX-class. Date and time of detections.
+#' @param group Character. Group identifier (transmitter, fish ID, etc.).
+#' @param stdate Date- or POSIX-class. Which day you want the time series to start?
+#'    Defaults to day of earliest detection in the data set. Must be the same
+#'    class as \code{enddate}
+#' @param enddate Date- or POSIX-class.
+#' @return Output is a data frame containing the number of each group remaining
+#'    at each date.
+#' @export
 
 trans_loss <- function(data, dates, group, stdate = NULL, enddate = NULL){
 
