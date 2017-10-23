981cc26ea267cf7a4067febc0eee4e9e8cac7c4d
mhpob <mikeob9@gmail.com>
2017-10-23 13:22:47 -0400

    Fix recursive directory issue. Comment certain steps.

--- R/UNIDprep.R
+++ R/UNIDprep.R
@@ -5,6 +5,7 @@ directory <- 'p:/obrien/biotelemetry/detections/cbl pier'
 UNIDprep <- function(unids, directory = getwd(), out = getwd()){
   Unks <- data.frame(Transmitters = unique(unids$transmitter))
 
+  # List VRL and VRL-RLD files
   vrls <- gsub('csv', 'vrl',
                    unique(unids$file))
   vrl_rlds <- strsplit(vrls, '_')
@@ -15,13 +16,19 @@ UNIDprep <- function(unids, directory = getwd(), out = getwd()){
   vrl_rlds <- paste(vrl_rld_front, vrl_rld_back, sep = '_')
   vrl_all <- c(vrls, vrl_rlds)
 
+  # Find location of files within directory
+  vrl_locs <- unlist(
+    lapply(vrl_all, function(x) list.files(path = directory, pattern = x,
+                                           recursive = T, full.names = T))
+    )
+
+  # Output UNID list, copy VRL and VRL-RLDs into new folder.
   output_location <- file.path(out, 'Unknown for VEMCO')
   dir.create(output_location)
   write.csv(Unks, file.path(output_location, 'unknown_ids.csv'),
             row.names = F)
 
-  #works for one folder, will run into recursive issues
-  file.copy(from = file.path(directory, vrl_all),
+  file.copy(from = vrl_locs,
             to = file.path(output_location, vrl_all))
 
 }
