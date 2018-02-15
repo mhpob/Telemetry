f8b6e50630865a08f365b59b6bd25ade403526b6
mhpob <mikeob9@gmail.com>
2018-02-15 11:04:49 -0500

    Fix bug that removes simultaneous detections at multiple receivers.

--- R/vemsort.R
+++ R/vemsort.R
@@ -57,7 +57,8 @@ vemsort <- function(directory = getwd()) {
   detects <- detects[, c('date.utc', 'date.local', 'receiver', 'transmitter',
                          'trans.name', 'trans.serial', 'sensor.value',
                          'sensor.unit', 'station', 'lat', 'long', 'file')]
-  detects <- detects[!duplicated(detects, by = c('date.utc', 'transmitter'),
+  detects <- detects[!duplicated(detects, by = c('date.utc', 'transmitter',
+                                                 'station'),
                            fromLast = T),]
   row.names(detects) <- NULL
   cat('Done.\n')
